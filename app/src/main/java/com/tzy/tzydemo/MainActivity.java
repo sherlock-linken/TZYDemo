27a2b7c00145338351ca62d91217483183002e04
靓仔勇 <tanzhiyong@aipai.com>
2019-01-30 17:23:03 +0800

    提交Demo列表界面

--- app/src/main/java/com/tzy/tzydemo/MainActivity.java
+++ app/src/main/java/com/tzy/tzydemo/MainActivity.java
@@ -1,29 +1,78 @@
 package com.tzy.tzydemo;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private ArrayList<String> dataList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        dataList.add("sherlock");
+        dataList.add("linken");
+
+        RecyclerView demoList = findViewById(R.id.rcy_demo_list);
+        DataAdapter adapter = new DataAdapter();
+        demoList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        demoList.setAdapter(adapter);
+
+    }
+
+
+    private class DataAdapter extends RecyclerView.Adapter<DemoItemViewHolder> {
+
+        @NonNull
+        @Override
+        public DemoItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new DemoItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_demo_list, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DemoItemViewHolder holder, int position) {
+
+            String data = dataList.get(position);
+
+            holder.title.setText(data);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
+    }
+
+
+    private class DemoItemViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title;
+
+
+        public DemoItemViewHolder(View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.tv_title);
+
+        }
     }
 
 }
